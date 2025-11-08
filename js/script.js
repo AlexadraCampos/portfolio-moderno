68148db8afcc3bcce198dea61c240dc19e12ce5a
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-11-08 20:06:26 -0300

    alteração imagem e css do script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
-// --- MENU MOBILE ---
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
 
 // --- GITHUB PROJECTS ---
 const username = "AlexadraCampos";
 const projetosSelecionados = [
   "Ponto-_de-_Luxo",
-  "portfolio-powerbi",
   "clinica",
-  "PaginaIDGE"
+  "PaginaIDGE",
+  "portfolio-powerbi",
 ];
 
 async function loadRepos() {
@@ -45,13 +39,13 @@ async function loadRepos() {
     div.classList.add('repo-list');
     div.innerHTML = `
       <a href="${repo.name === "portfolio-powerbi" ? "https://portfolio-powerbi.netlify.app/" : repo.html_url}" target="_blank">
-        <h1>${repo.name}</h1>
+        <h1>${repo.name.replace(/[-_]/g, ' ')}</h1>
         ${imageURL ? `<img src="${imageURL}" alt="Preview do projeto ${repo.name}">` : ''}
         <p>${repo.description || 'Clique na imagem para ver mais detalhes'}</p>
       </a>
     `;
 
-    // se for o projeto de Power BI, usa o link do site
+    // Power BI, usa o link do site
     const projectLink = repo.name === "portfolio-powerbi"
     ? "https://portfolio-powerbi.netlify.app/" 
     : repo.html_url;
